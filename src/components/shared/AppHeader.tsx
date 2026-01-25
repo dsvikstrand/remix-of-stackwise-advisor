f06aaa5bf07284af2852edf112b5bf09e1a43964
v1kstrand <david.vikstrand@example.com>
2026-01-25 11:04:08 +0100

    refactor(AppHeader): update debugTag value

--- src/components/shared/AppHeader.tsx
+++ src/components/shared/AppHeader.tsx
@@ -14,7 +14,7 @@ interface AppHeaderProps {
 export function AppHeader({ actions, showFloatingNav = true }: AppHeaderProps) {
   const { user } = useAuth();
   const navMode = user ? 'all' : 'public';
-  const debugTag = 'abc125';
+  const debugTag = 'xxx123';
 
   return (
     <>
